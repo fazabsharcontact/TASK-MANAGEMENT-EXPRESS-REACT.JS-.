c2550ce80c123cb08dd7f33861e664913ec03a2d
fazaabsharcontact <fazaabsharcontact@gmail.com>
2026-03-30 22:24:25 +0700

    create addTask function(note: failed to add task)

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
     const [tasks, setTasks] = useState([]);
+    const [title, setTitle] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -43,11 +44,55 @@ function Dashboard() {
         fetchTasks();
     }, []);
 
+    const handleAddTask = async (e) => {
+        e.preventDefault();
+
+        try {
+            const token = localStorage.getItem("token");
+
+            const response = await fetch("http://localhost:3000/api/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({ title }),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                alert(data.message || "Gagal tambah task");
+                return;
+            }
+
+            setTasks([...tasks, data]);
+
+            setTitle("");
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">
                 Dashboard
             </h1>
+
+            <form onSubmit={handleAddTask} className="mb-4">
+                <input 
+                    type="text"
+                    placeholder="Add Task..."
+                    className="border p-2 mr-2 rounded w-64"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                    Add
+                </button>
+            </form>
 
             {tasks.length === 0 ? (
                 <p>Tidak ada task</p>
